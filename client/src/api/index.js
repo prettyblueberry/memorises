96d09d0d8b0567e144f4f67e1bd5250728cfeef7
blueberry <121964338+prettyblueberry@users.noreply.github.com>
2021-05-16 22:00:39 +0530

    updated index page logo and title

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API = axios.create({ baseURL: "https://mern-stack-app-memories.herokuapp.com" });
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://mern-stack-app-memories.herokuapp.com" });
+// const API = axios.create({ baseURL: "http://localhost:5000" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
